78d7acc0b300d0d115e0c0569e27a3a1f284a269
Connie Ip <connie.ip@cargosmart.com>
2018-12-05 14:16:28 +0800

    put todos to redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'
+import reducer from './reducer';
+import TodoList from './components/TodoList.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <TodoList/>
+    </Provider>, document.getElementById('root'));
